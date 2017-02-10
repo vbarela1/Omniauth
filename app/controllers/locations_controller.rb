6dc4b553a288a2eb324049fb422c1fad79ac68c7
Valerie Barela <vbarela1@gmail.com>
2017-02-10 15:08:41 -0700

    third commit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,54 @@
 class LocationsController < ApplicationController
+  before_action :set_trip
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @locations = @trip.locations 
+  end 
+  
   def show
   end
 
   def new
+    @location = @trip.locations.new 
   end
+
+  def create
+    @location = @trip.locations.new(location_params)
+    if @location.save
+     redirect_to trip_path(@trip)
+    else
+      render :new 
+    end 
+  end 
 
   def edit
   end
+
+  def update
+    if @location.update(location_params)
+      redirect_to trip_location_path(@trip, @location)
+    else 
+      render :edit 
+    end 
+  end
+
+  def destory
+    @location.destroy
+    redirect_to trip_locations_path(@trip)
+  end
+
+  private 
+
+  def location_params
+    params.require(:location).permit(:description)
+  end 
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end  
+
+  def set_location
+    @location = @trip.locations.find(params[:id])
+  end 
 end
